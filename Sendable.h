3bfff1ebc4998da3860e5c3da0e366cb7f773d19
Kevin <magneticok@gmail.com>
2019-05-09 03:41:30 -0600

    JSON Manager

--- Sendable.h
+++ Sendable.h
@@ -5,26 +5,49 @@
 #ifndef GLADIATORS_SENDABLE_H
 #define GLADIATORS_SENDABLE_H
 
-#include <algorithm>
-#include <boost/property_tree/json_parser.hpp>
-#include <vector>
-#include <boost/algorithm/string.hpp>
 #include <string>
 #include "Gladiator.h"
-#include <string>
+#include <iostream>
+#include <cstring>
 #include "LinkedList.h"
 
 using namespace std;
-using boost::property_tree::ptree;
 
+/**
+ * Clase paquete que se va a enviar entre el servidor y el cliente.
+ * @author elorim
+ * */
 class Sendable {
 public:
     Sendable();
+    /**
+     * Transforma el objeto actual a un string tipo json
+     * @returns String tipo json
+     * @author elorim
+     * */
+    string toJson();
+    /**
+     * Transforma el string pasado a un objeto de tipo sendable
+     * @param json String en formato json a convertir
+     * @returns Objeto sendable basado en el JSON.
+     * @author elorim
+     * */
+    static Sendable toObj(const char json[]);
+
+    // Getters & Setters
+    LinkedList<LinkedList<int>> getMovimientos(){return movimientos;}
+    string getMensaje(){return mensaje;}
+    void setMovimientos(LinkedList<LinkedList<int>> lista){movimientos=lista;}
+    void setMensaje(string msj){mensaje=msj;}
+    void setGlad1(int glad1[4]);
+    void setGlad2(int glad2[4]);
+
 private:
-    Gladiator glad1;
-    Gladiator glad2;
-    LinkedList<LinkedList<int>> movimientos; // 0000 : (xf)(yf)(xi)(yi)
-    string mensaje;
+
+    LinkedList<LinkedList<int>> movimientos; /** La lista de movimientos a enviar con formato, 0000 : (xf)(yf)(xi)(yi)*/
+    string mensaje;  /**Un mensaje*/
+    int glad1[4]; /** Array de integers del gladiador 1, A**/
+    int glad2[4]; /**Array de integers del gladiador 2, Backtracking*/
 };
 
 
